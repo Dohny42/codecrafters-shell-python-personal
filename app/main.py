d33415477cd58c949be4a0134b11d5edaca0d131
Dohny42 <dohny42@gmail.com>
2026-05-25 20:24:29 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
+import os
 import sys
 
-VALID_COMMANDS = ["exit", "echo", "type"]
+
+def handle_echo(args: str):
+    if len(args) > 1:
+        print(args[1])
+    else:
+        print()
+
+
+def check_executable_exists(command: str) -> tuple[bool, str]:
+    for path in sys.path:
+        executable_path = f"{path}/{command}"
+        if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+            return True, executable_path
+        else:
+            continue
+    return False, ""
+
+
+def handle_type(args: str):
+    # currently will not handle multiple args or missing args, just assume the good case
+    if args in BUILTIN_COMMAND_HANDLER_MAP or args == "exit":
+        print(f"{args} is a shell builtin")
+    exe_exist, full_path = check_executable_exists(args)
+    if exe_exist:
+        print(f"{args} is {full_path}")
+    else:
+        print(f"{args}: not found")
+
+
+BUILTIN_COMMAND_HANDLER_MAP = {"echo": handle_echo, "type": handle_type}
 
 
 def main():
@@ -10,25 +40,15 @@ def main():
         # handle exit
         if user_input == "exit":
             break
-        # handle echo
+        # handle builtins
         user_input_split = user_input.split(" ", maxsplit=1)
-        if user_input_split[0] == "echo":
-            if len(user_input_split) > 1:
-                print(user_input_split[1])
-            else:
-                print()
+        if user_input_split[0] in BUILTIN_COMMAND_HANDLER_MAP:
+            handler = BUILTIN_COMMAND_HANDLER_MAP[user_input_split[0]]
+            handler(user_input_split[1])
             continue
-        # handle type
-        if user_input_split[0] == "type":
-            # currently will not handle multiple args or missing args, just assume the good case
-            if user_input_split[1] in VALID_COMMANDS:
-                print(f"{user_input_split[1]} is a shell builtin")
-                continue
-            else:
-                print(f"{user_input_split[1]}: not found")
-                continue
         # handle unknown command
-        print(f"{user_input}: command not found")
+        else:
+            print(f"{user_input}: command not found")
 
 
 if __name__ == "__main__":
